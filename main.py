de0081778bdfe10479d3ffc0ae59a38b84830c91
Divyesh Sharma <drshar79>
2020-03-05 10:39:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
 import requests
-def Choice():
-    print("Do you want to find info about earthquakes(1), or amount of people in space(2)?")
-    ans=input()
-    if ans=="1":
-        print(res2)
-    elif ans=="2":
-        print(res)
-    else: 
-        incorrect
-
-def incorrect():
-    print("That isn't one of your choices!")  
-    Choice()
 
 
 URL = "http://api.open-notify.org/astros.json"
@@ -19,7 +6,4 @@ URL = "http://api.open-notify.org/astros.json"
 res = requests.get(URL) # get the data
 res = res.json() # convert data to Python format
 
-URL2= "https://earthquake.usgs.gov/fdsnws/event/1/application.json"
-res2 = requests.get(URL2)
-res2=res2.json()
-Choice()
+print(res)
