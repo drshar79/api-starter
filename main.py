263f26c4c116064af8181a75cbd44d3d374df482
Divyesh Sharma <drshar79>
2020-03-05 11:01:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,27 +5,10 @@ URL = "http://api.open-notify.org/astros.json"
 
 res = requests.get(URL) # get the data
 res = res.json() # convert data to Python format
-data = {
-  "message": "success",
-  "number": 3,
-  "people": [
-    {
-      "name": "Andrew Morgan",
-      "craft": "ISS"
-    },
-    {
-      "name": "Oleg Skripochka",
-      "craft": "ISS"
-    },
-    {
-      "name": "Jessica Meir",
-      "craft": "ISS"
-    },
-  ]
-}
+
 
 def choice():
-    print(data["people"])
+    print(res["people"])
     print("Would you like to see information about earthquakes?",
     "Yes or ",
     "No",
@@ -36,4 +19,4 @@ def choice():
 URL2= "https://earthquake.usgs.gov/fdsnws/event/1/application.json"
 res2 = requests.get(URL2)
 res2 = res2.json()
-#print(res2)
+choice()
